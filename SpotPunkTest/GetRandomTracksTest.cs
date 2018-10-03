50e3df383b26162f09d9ab9de869ea0f4ca1f920
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-10-03 14:51:19 -0500

    Added static variables to the test file

--- SpotPunkTest/GetRandomTracksTest.cs
+++ SpotPunkTest/GetRandomTracksTest.cs
@@ -25,6 +25,7 @@ namespace SpotPunkTest
 
         private static readonly string ExampleOutput = "{\"tracks\":{\"href\":\"https://api.spotify.com/v1/search?query=cold&type=track&market=US&offset=85&limit=1\",\"items\":[{\"album\":{\"album_type\":\"single\",\"artists\":[{\"external_urls\":{\"spotify\":\"https://open.spotify.com/artist/2RJWS2Lmkw2uExDmFMe1Ry\"},\"href\":\"https://api.spotify.com/v1/artists/2RJWS2Lmkw2uExDmFMe1Ry\",\"id\":\"2RJWS2Lmkw2uExDmFMe1Ry\",\"name\":\"LiL Lotus\",\"type\":\"artist\",\"uri\":\"spotify:artist:2RJWS2Lmkw2uExDmFMe1Ry\"}],\"available_markets\":[\"AD\",\"AR\",\"AT\",\"AU\",\"BE\",\"BG\",\"BO\",\"BR\",\"CA\",\"CH\",\"CL\",\"CO\",\"CR\",\"CY\",\"CZ\",\"DE\",\"DK\",\"DO\",\"EC\",\"EE\",\"ES\",\"FI\",\"FR\",\"GB\",\"GR\",\"GT\",\"HK\",\"HN\",\"HU\",\"ID\",\"IE\",\"IL\",\"IS\",\"IT\",\"JP\",\"LI\",\"LT\",\"LU\",\"LV\",\"MC\",\"MT\",\"MX\",\"MY\",\"NI\",\"NL\",\"NO\",\"NZ\",\"PA\",\"PE\",\"PH\",\"PL\",\"PT\",\"PY\",\"RO\",\"SE\",\"SG\",\"SK\",\"SV\",\"TH\",\"TR\",\"TW\",\"US\",\"UY\",\"VN\",\"ZA\"],\"external_urls\":{\"spotify\":\"https://open.spotify.com/album/105Aso4HZFWGJQUuF4HV9u\"},\"href\":\"https://api.spotify.com/v1/albums/105Aso4HZFWGJQUuF4HV9u\",\"id\":\"105Aso4HZFWGJQUuF4HV9u\",\"images\":[{\"height\":640,\"url\":\"https://i.scdn.co/image/474ff912720d0036fc4b82a9509336a630c2e651\",\"width\":640},{\"height\":300,\"url\":\"https://i.scdn.co/image/afdcfcb491f00f9a0e86f87d1b8b4c88a69c47c0\",\"width\":300},{\"height\":64,\"url\":\"https://i.scdn.co/image/d178d8b77bc5ca364f84a2251c4a695cc3ad85fb\",\"width\":64}],\"name\":\"Bodybag\",\"release_date\":\"2017-07-07\",\"release_date_precision\":\"day\",\"total_tracks\":6,\"type\":\"album\",\"uri\":\"spotify:album:105Aso4HZFWGJQUuF4HV9u\"},\"artists\":[{\"external_urls\":{\"spotify\":\"https://open.spotify.com/artist/2RJWS2Lmkw2uExDmFMe1Ry\"},\"href\":\"https://api.spotify.com/v1/artists/2RJWS2Lmkw2uExDmFMe1Ry\",\"id\":\"2RJWS2Lmkw2uExDmFMe1Ry\",\"name\":\"LiL Lotus\",\"type\":\"artist\",\"uri\":\"spotify:artist:2RJWS2Lmkw2uExDmFMe1Ry\"},{\"external_urls\":{\"spotify\":\"https://open.spotify.com/artist/1fsCfvdiomqjKJFR6xI8e4\"},\"href\":\"https://api.spotify.com/v1/artists/1fsCfvdiomqjKJFR6xI8e4\",\"id\":\"1fsCfvdiomqjKJFR6xI8e4\",\"name\":\"Cold Hart\",\"type\":\"artist\",\"uri\":\"spotify:artist:1fsCfvdiomqjKJFR6xI8e4\"},{\"external_urls\":{\"spotify\":\"https://open.spotify.com/artist/0lGIw6TiVbdnlQFpUGleOY\"},\"href\":\"https://api.spotify.com/v1/artists/0lGIw6TiVbdnlQFpUGleOY\",\"id\":\"0lGIw6TiVbdnlQFpUGleOY\",\"name\":\"Nedarb\",\"type\":\"artist\",\"uri\":\"spotify:artist:0lGIw6TiVbdnlQFpUGleOY\"}],\"available_markets\":[\"AD\",\"AR\",\"AT\",\"AU\",\"BE\",\"BG\",\"BO\",\"BR\",\"CA\",\"CH\",\"CL\",\"CO\",\"CR\",\"CY\",\"CZ\",\"DE\",\"DK\",\"DO\",\"EC\",\"EE\",\"ES\",\"FI\",\"FR\",\"GB\",\"GR\",\"GT\",\"HK\",\"HN\",\"HU\",\"ID\",\"IE\",\"IL\",\"IS\",\"IT\",\"JP\",\"LI\",\"LT\",\"LU\",\"LV\",\"MC\",\"MT\",\"MX\",\"MY\",\"NI\",\"NL\",\"NO\",\"NZ\",\"PA\",\"PE\",\"PH\",\"PL\",\"PT\",\"PY\",\"RO\",\"SE\",\"SG\",\"SK\",\"SV\",\"TH\",\"TR\",\"TW\",\"US\",\"UY\",\"VN\",\"ZA\"],\"disc_number\":1,\"duration_ms\":283404,\"explicit\":false,\"external_ids\":{\"isrc\":\"QM42K1783407\"},\"external_urls\":{\"spotify\":\"https://open.spotify.com/track/48AnfHcY01HOX0lHngb8ir\"},\"href\":\"https://api.spotify.com/v1/tracks/48AnfHcY01HOX0lHngb8ir\",\"id\":\"48AnfHcY01HOX0lHngb8ir\",\"is_local\":false,\"name\":\"Bodybag (Feat. cold hart)\",\"popularity\":55,\"preview_url\":\"https://p.scdn.co/mp3-preview/2f899c5f07d23ee14edbae90c4cfdf53b7f0cc3a?cid=774b29d4f13844c495f206cafdad9c86\",\"track_number\":1,\"type\":\"track\",\"uri\":\"spotify:track:48AnfHcY01HOX0lHngb8ir\"}],\"limit\":1,\"next\":\"https://api.spotify.com/v1/search?query=cold&type=track&market=US&offset=86&limit=1\",\"offset\":85,\"previous\":\"https://api.spotify.com/v1/search?query=cold&type=track&market=US&offset=84&limit=1\",\"total\":95548}}";
         private static readonly string UnauthorizedOutput = "Unauthorized fool.";
+        private static readonly string UserToken = "UserToken";
 
         #endregion
 
@@ -69,7 +70,7 @@ namespace SpotPunkTest
             // set request header
             request.Setup(req => req.Headers).Returns(new HeaderDictionary(new Dictionary<string, StringValues>()
             {
-                ["token"] = "UserTokenGoesHere"
+                ["token"] = UserToken
             }
             ));
 
@@ -103,7 +104,7 @@ namespace SpotPunkTest
             // set request header
             request.Setup(req => req.Headers).Returns(new HeaderDictionary(new Dictionary<string, StringValues>()
             {
-                ["token"] = "UserTokenGoesHere"
+                ["token"] = UserToken
             }
             ));
 
